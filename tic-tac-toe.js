57553433bcf523cfa654a15f1c6ec3dfebfca3d2
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-03-18 23:01:56 +0200

    Added elements to DOM. Grid tiles are named after their index. Changed gameboard from 2D to 1D, requiring changes to the 3-in-a-row checks

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,46 +1,76 @@
 const theGameBoard = (function(){
     const gameBoard = [
-        ["","",""],
-        ["","",""],
-        ["","",""]]
+        "","","",
+        "","","",
+        "","",""]
     
     const addToBoard = (thing) => gameBoard.push(thing)
     //TODO: Check board for every win condition
     const playerWins = (symbol) => {
         //Check lines for symbol
-        for (let i = 0; i < gameBoard.length; i++) {
-            // Check across
-            if (gameBoard[i][0] == symbol && 
-                gameBoard[i][1] == symbol && 
-                gameBoard[i][2]== symbol) {
-                    return true
-            }
-
-            // Check Col
-            if (gameBoard[0][i]==symbol &&
-                gameBoard[1][i] == symbol &&
-                gameBoard[2][i] == symbol) {
-                    return true
-            }
-        } 
         // Check Diagonals    
-        if (gameBoard[0][0] == symbol &&
-            gameBoard[1][1] == symbol &&
-            gameBoard[2][2] == symbol) {
+        if (gameBoard[0] == symbol &&
+            gameBoard[4] == symbol &&
+            gameBoard[8] == symbol) {
                 return true
         }
-        if (gameBoard[0][2] == symbol &&
-            gameBoard[1][1] == symbol &&
-            gameBoard[2][0] == symbol) {
+        if (gameBoard[2]== symbol &&
+            gameBoard[4]== symbol &&
+            gameBoard[6]== symbol) {
+                return true
+        }
+        // Check across
+        if ((gameBoard[0] == symbol && 
+            gameBoard[1] == symbol && 
+            gameBoard[2]== symbol) ||
+            
+            (gameBoard[3] == symbol && 
+            gameBoard[4] == symbol && 
+            gameBoard[5]== symbol) ||
+            
+            (gameBoard[6] == symbol && 
+            gameBoard[7] == symbol && 
+            gameBoard[8]== symbol)) {
+                return true
+        }
+        // Check Columns
+        if ((gameBoard[0]==symbol &&
+            gameBoard[3] == symbol &&
+            gameBoard[6] == symbol)||
+            
+            (gameBoard[1]==symbol &&
+            gameBoard[4] == symbol &&
+            gameBoard[7] == symbol) ||
+            
+            (gameBoard[2]==symbol &&
+            gameBoard[5] == symbol &&
+            gameBoard[8] == symbol)) {
                 return true
         }
     }
     return {gameBoard}
 })
-//TODO: displayControl
+//TODO: gameControl
+const gameControl = (function() {
+    player_1 = createPlayer()
+    player_2 = createPlayer()
+    
+    //TODO: Add ability to alternate between players
+    if (playerWins('x')) {
+        console.log(`${player_1.name} wins!`)
+    }
+    if (playerWins('o')) {
+        console.log(`${player_2.name} wins!`)
+    }
+    
+})
 //TODO: Player factory
-function createPlayer() {
+function createPlayer(name) {
     // Player attributes
-    return { }
+    this.name = name
+    this.score = 0
+    
+    const increaseScore = () => {this.score++}
+    return { name, score }
 }
 //TODO: Create playing grid with each cell having coordinates corresponding to gameboard array
