7a111e702d4b8c2360ffe246f1d12f60cf348fc6
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-03-19 22:08:21 +0200

    added shells for playes and displayController. Added render method to gameBoard to set innerhtml of tiles to the tiles array

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,34 @@
 // Event Listeners
 const resetButton = document.querySelector("#reset-btn")
-resetButton.addEventListener("click", gameBoard.reset())
+resetButton.addEventListener("click", () =>{
+    gameBoard.reset()
+})
 
 // Factories
-
+function createPlayer(name) {
+    this.name = name
+    
+}
 
 // Modules
 const gameBoard = (function (){
-    const tiles = Array(9).fill(null)
+    let tiles = Array(9).fill("")
     const reset = () => {
-        // reset the gamboard array and the tiles
+        // reset the gameboard array and the tiles
+        tiles = tiles.fill(null)
     }
+
+    const render = () => {
+        // put contents of tiles into html gameboard
+        for (let i = 0; i < tiles.length; i++) {
+            let tileElement = document.querySelector(`#${i}`)
+            tileElement.innerHTML = tiles[i]
+        }
+    }
+
+    return { reset}
+})()
+
+const displayController = (function () {
+
 })
